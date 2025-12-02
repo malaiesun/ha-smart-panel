bea3092ff2db1e01363a793cef6c3fdff02e8ed6
Malaiesun <59860704+malaiesun@users.noreply.github.com>
2025-12-02 23:27:54 +0530

    FIxed

--- custom_components/mesh_panel/config_flow.py
+++ custom_components/mesh_panel/config_flow.py
@@ -1,11 +1,31 @@
 import voluptuous as vol
 from homeassistant import config_entries
 
-from .const import DOMAIN, CONF_PANEL_ID
+from .const import DOMAIN, CONF_PANEL_ID, CONF_MANUAL_CONFIG
 
 
-class MeshPanelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class MeshPanelOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        current = self.config_entry.options.get(CONF_MANUAL_CONFIG, "")
+        schema = vol.Schema({
+            vol.Optional(CONF_MANUAL_CONFIG, default=current): str
+        })
+        return self.async_show_form(step_id="init", data_schema=schema)
+
+
+@config_entries.HANDLERS.register(DOMAIN)
+class MeshPanelConfigFlow(config_entries.ConfigFlow):
     VERSION = 1
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return MeshPanelOptionsFlowHandler(config_entry)
 
     async def async_step_user(self, user_input=None):
         errors = {}
